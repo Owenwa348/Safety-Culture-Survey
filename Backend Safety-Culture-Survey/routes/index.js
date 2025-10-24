c027616049e08c460452a087f5cf5a9328b567e4
นาย ธันวา ชัยรัตนานนท์ (Tanwa Chairattananon) <141398800+Owenwa348@users.noreply.github.com>
2025-10-24 16:02:51 +0700

    + ทดสอบการดึงตัวคำถาม

--- Backend Safety-Culture-Survey/routes/index.js
+++ Backend Safety-Culture-Survey/routes/index.js
@@ -8,6 +8,7 @@ const workGroupRoutes = require('./Workgroup/workGroupRoutes');
 const categoryRoutes = require('./Category/categoryRoutes');
 const questionRoutes = require('./Questions/questionRoutes');
 const userRoutes = require('./User/userRoutes');
+const assessmentRoutes = require("./assessmentRoutes");
 
 const registerRoutes = (app) => {
   app.use('/excel-upload', excelUploadRouter);
@@ -20,6 +21,7 @@ const registerRoutes = (app) => {
   app.use('/api/questions', questionRoutes);
   app.use('/api/super-admins', superAdminRoutes);
   app.use('/api/users', userRoutes);
+  app.use("/api/assessment", assessmentRoutes);
 };
 
 module.exports = registerRoutes;
