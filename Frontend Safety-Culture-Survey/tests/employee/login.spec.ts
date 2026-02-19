f712db7344c1d9cf680206faf078b829eef4454c
นาย ธันวา ชัยรัตนานนท์ (Tanwa Chairattananon) <141398800+Owenwa348@users.noreply.github.com>
2026-02-19 14:43:31 +0700

    feat: Update test cases with new user credentials and add Take Assessment test

--- Frontend Safety-Culture-Survey/tests/employee/login.spec.ts
+++ Frontend Safety-Culture-Survey/tests/employee/login.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
   await page.getByRole('textbox', { name: 'name@company.com' }).click();
-  await page.getByRole('textbox', { name: 'name@company.com' }).fill('somchai.jai@example.com');
+  await page.getByRole('textbox', { name: 'name@company.com' }).fill('assessor@tunwa.com');
   await page.getByRole('button', { name: 'ดำเนินการต่อ' }).click();
   await page.getByRole('textbox', { name: 'กรอกรหัสผ่านของคุณ' }).click();
-  await page.getByRole('textbox', { name: 'กรอกรหัสผ่านของคุณ' }).fill('User@123');
+  await page.getByRole('textbox', { name: 'กรอกรหัสผ่านของคุณ' }).fill('Admin@123');
   await page.getByRole('button', { name: 'เข้าสู่ระบบ' }).click();
 });
